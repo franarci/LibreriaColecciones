da314f2d80688696dc6689c4b0866e88be6f0d86
franarci <fran.arciniega96@gmail.com>
2021-10-20 18:54:12 -0300

    Precarga e implemento boton guardar

--- WindowsList/frmEmpleados.Designer.cs
+++ WindowsList/frmEmpleados.Designer.cs
@@ -37,7 +37,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.grdEmpleados = new System.Windows.Forms.DataGridView();
             this.btnGuardar = new System.Windows.Forms.Button();
-            this.btnTraerPorCiudad = new System.Windows.Forms.Button();
+            this.btnTraerPorId = new System.Windows.Forms.Button();
             this.txtNacionalidad = new System.Windows.Forms.TextBox();
             this.txtFechaNac = new System.Windows.Forms.TextBox();
             this.txtCiudad = new System.Windows.Forms.TextBox();
@@ -45,6 +45,7 @@
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.txtId = new System.Windows.Forms.TextBox();
             this.lblAutor = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.grdEmpleados)).BeginInit();
             this.SuspendLayout();
             // 
@@ -121,21 +122,22 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(410, 205);
+            this.btnGuardar.Location = new System.Drawing.Point(300, 205);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(218, 23);
+            this.btnGuardar.Size = new System.Drawing.Size(203, 23);
             this.btnGuardar.TabIndex = 25;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
-            // btnTraerPorCiudad
+            // btnTraerPorId
             // 
-            this.btnTraerPorCiudad.Location = new System.Drawing.Point(155, 205);
-            this.btnTraerPorCiudad.Name = "btnTraerPorCiudad";
-            this.btnTraerPorCiudad.Size = new System.Drawing.Size(220, 23);
-            this.btnTraerPorCiudad.TabIndex = 24;
-            this.btnTraerPorCiudad.Text = "Traer Por Ciudad";
-            this.btnTraerPorCiudad.UseVisualStyleBackColor = true;
+            this.btnTraerPorId.Location = new System.Drawing.Point(61, 205);
+            this.btnTraerPorId.Name = "btnTraerPorId";
+            this.btnTraerPorId.Size = new System.Drawing.Size(220, 23);
+            this.btnTraerPorId.TabIndex = 24;
+            this.btnTraerPorId.Text = "Traer Por Id";
+            this.btnTraerPorId.UseVisualStyleBackColor = true;
             // 
             // txtNacionalidad
             // 
@@ -189,11 +191,21 @@
             this.lblAutor.TabIndex = 17;
             this.lblAutor.Text = "Empleado";
             // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(522, 205);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(218, 23);
+            this.btnEliminar.TabIndex = 34;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
             // frmEmpleados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -203,7 +215,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.grdEmpleados);
             this.Controls.Add(this.btnGuardar);
-            this.Controls.Add(this.btnTraerPorCiudad);
+            this.Controls.Add(this.btnTraerPorId);
             this.Controls.Add(this.txtNacionalidad);
             this.Controls.Add(this.txtFechaNac);
             this.Controls.Add(this.txtCiudad);
@@ -231,7 +243,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView grdEmpleados;
         private System.Windows.Forms.Button btnGuardar;
-        private System.Windows.Forms.Button btnTraerPorCiudad;
+        private System.Windows.Forms.Button btnTraerPorId;
         private System.Windows.Forms.TextBox txtNacionalidad;
         private System.Windows.Forms.TextBox txtFechaNac;
         private System.Windows.Forms.TextBox txtCiudad;
@@ -239,5 +251,6 @@
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.Label lblAutor;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
--- WindowsList/frmEmpleados.cs
+++ WindowsList/frmEmpleados.cs
@@ -26,7 +26,26 @@ namespace WindowsList
 
         private void precarga()
         {
-           
+            empleados.AddRange(new List<Empleado> {
+                 new Empleado(0, "Martinez","Pedro", new DateTime(1990,04,04), "CABA", "Argentina"),
+                 new Empleado(1, "Fernandez", "Carla", new DateTime(1947,09,21),"La Plata", "Argentina"),
+                 new Empleado(2, "Santos", "Gustavo", new DateTime(1970,06,13),"Ezeiza", "Argentina")
+            });
+
+            grdEmpleados.DataSource = empleados;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            empleados.Add(new Empleado(Convert.ToInt32(txtId.Text), txtApellido.Text, txtNombre.Text, Convert.ToDateTime(txtFechaNac.Text), txtCiudad.Text, txtNacionalidad.Text));
+
+            actualizarGrilla();
+        }
+
+        public void actualizarGrilla()
+        {
+            grdEmpleados.DataSource = null;
+            grdEmpleados.DataSource = empleados;
         }
     }
 }
