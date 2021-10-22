cb4bb08e04585d44a97e4337fbaeb27190b17ab1
franarci <fran.arciniega96@gmail.com>
2021-10-21 22:51:04 -0300

    Admin publicadores

---
+++ Entidades/Models/Admin/AdminPublicadores.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Models.Admin
+{
+    public static class AdminPublicadores
+    {
+        public static List<Publicador> publicadores = new List<Publicador>()
+            {
+                new Publicador(0, "Fernandez", "Pedro", new DateTime(1995,10,5),"La Plata"),
+                new Publicador(1, "Hernandez", "Florencia", new DateTime(1996,09,20),"Los Hornos")
+            };
+
+        public static List<Publicador> Listar()
+        {
+            return publicadores;
+        }
+
+        public static void Insertar(Publicador p)
+        {
+            publicadores.Add(p);
+        }
+
+        public static void Eliminar(int id)
+        {
+            Publicador p = publicadores.Find(publicador => publicador.Id == id);
+            publicadores.Remove(p);
+        }
+    }
+}
